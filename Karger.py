ebc216c4b07fa3013f1924a25b061c5b4a05c00a
patricknickols <patricknickols@gmail.com>
2024-02-15 11:35:31 +0000

    Fix bug and switch to list-based implementation

--- Karger.py
+++ Karger.py
@@ -1,36 +1,36 @@
 import random
+
 def get_edges_from_dictionary(adjacency_dictionary):
-    edges = set()
+    edges = []
     for key in adjacency_dictionary:
         for item in adjacency_dictionary[key]:
-            if (item,key) not in edges:
-                edges.add((key, item)) 
+            if (item, key) not in edges:
+                edges.append((key, item)) 
     return edges
 
 def get_edges_from_adj_matrix(adj):
-    return [(i,j) for i in range(len(adj) for j in range(len(adj[0]))) if adj[i][j] != 0]
-
+    edges = []
+    for i in range(len(adj)):
+        for j in range(len(adj[0])):
+            if adj[i][j] != 0:
+                edges.append((i,j)) 
+    return edges
 
 def contract(edges, edge):
     source = edge[0]
     target = edge[1]
     new_node_label = f"{source},{target}"
-    edges.discard((source, target))
-    edges.discard((target, source))
-    removals = set()
-    additions = set()
+    edges = [edge for edge in edges if edge != (source, target)]
+    edges = [edge for edge in edges if edge != (target, source)]
+    new_edges = []
     for other_source, other_target in edges: 
         if other_source in {source, target}:
-            removals.add((other_source, other_target))
-            additions.add((new_node_label, other_target))
+            new_edges.append((new_node_label, other_target))
         elif other_target in {source, target}:
-            removals.add((other_source, other_target))
-            additions.add((other_source, new_node_label))
-    edges -= removals
-    edges |= additions
-    return edges
-
-        
+            new_edges.append((other_source, new_node_label))
+        else:
+            new_edges.append((other_source, other_target))
+    return new_edges
 
 def number_of_nodes(edges):
     node_set = set()
@@ -41,9 +41,9 @@ def number_of_nodes(edges):
         
 def _karger(edges):
     while number_of_nodes(edges) > 2:
-        e = random.choice(list(edges))
+        e = random.choice(edges)
         edges = contract(edges, e)
-    return list(edges)[0]
+    return list(edges)
 
 
 def karger_dict(adjacency_dictionary):
@@ -53,15 +53,3 @@ def karger_dict(adjacency_dictionary):
 def karger_mat(adjacency_matrix):
     edges = get_edges_from_adj_matrix(adjacency_matrix)
     return _karger(edges)
-
-
-
-
-test_graph_dictionary = {1: {2,4,5}, 
-                         2: {1,3,4}, 
-                         3: {2,4},
-                         4: {1,2,3,5},
-                         5: {1,4}
-                        }
-
-print(karger_dict(test_graph_dictionary))
